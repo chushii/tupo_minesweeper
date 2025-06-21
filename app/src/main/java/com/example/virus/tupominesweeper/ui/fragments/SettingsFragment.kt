555ae673147e5fcf9e247c247208e36a5c156c3a
chushii <libadushek@gmail.com>
2025-06-21 12:09:01 +0300

    settings preview

--- app/src/main/java/com/example/virus/tupominesweeper/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/virus/tupominesweeper/ui/fragments/SettingsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.virus.tupominesweeper.databinding.FragmentSettingsBinding
@@ -24,7 +25,15 @@ class SettingsFragment : Fragment() {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        binding.textSettings.text = "Экран настроек"
+        val diffs = arrayOf("Легкий", "Средний", "Сложный", "Пользовательский")
+        val diff_adapter = ArrayAdapter(requireActivity(), android.R.layout.simple_spinner_item, diffs)
+        diff_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.DiffChoice.setAdapter(diff_adapter)
+
+        val themes = arrayOf("Системная", "Светлая", "Тёмная")
+        val theme_adapter = ArrayAdapter(requireActivity(), android.R.layout.simple_spinner_item, themes)
+        theme_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.ThemeChoice.setAdapter(theme_adapter)
 
         return root
     }
