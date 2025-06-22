1e6c2da70a23d5acc18bbd3f7eb7b86d5a8003ff
chushii <libadushek@gmail.com>
2025-06-22 13:28:11 +0300

    leaderboard preview

--- app/src/main/java/com/example/virus/tupominesweeper/ui/fragments/LeaderboardFragment.kt
+++ app/src/main/java/com/example/virus/tupominesweeper/ui/fragments/LeaderboardFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -24,7 +25,10 @@ class LeaderboardFragment : Fragment() {
         _binding = FragmentLeaderboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        binding.textLeaderboard.text = "Экран лидерборда"
+        val diffs = arrayOf("Легкий", "Средний", "Сложный")
+        val diff_adapter = ArrayAdapter(requireActivity(), android.R.layout.simple_spinner_item, diffs)
+        diff_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.DiffChoice.setAdapter(diff_adapter)
 
         return root
     }
